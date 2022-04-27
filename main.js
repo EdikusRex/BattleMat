8892ec059e753917b8e9dfb29f54e045efe3f679
Edik Chekhanovsky <edichek12@hotmail.com>
2022-04-26 20:44:38 -0700

    Code merge corrections, accordion wip

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 const canvas = document.getElementById("canvas")
-canvas.height = window.innerHeight
-canvas.width = window.innerWidth
-
 const ctx = canvas.getContext("2d")
 
 let prevX = null
 let prevY = null
 let drawing = false
 let lines = [, ]
+
+canvas.height = window.innerHeight
+canvas.width = window.innerWidth
 ctx.lineWidth = 5
 
 // canvas.style.backgroundImage = "url('Assets/Maps/Black Peaks.jpg')"
 
+
+// ---------- Button Init ---------- //
 let clrs = document.querySelectorAll(".clr")
 clrs = Array.from(clrs)
 clrs.forEach(clr => {
@@ -23,12 +25,31 @@ clrs.forEach(clr => {
 let clearBtn = document.querySelector(".clear")
 clearBtn.addEventListener("click", () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-})
+});
+// ---------- Button Init ---------- //
+
+// ---------- Dropdown Init ---------- //
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+// ---------- Dropdown Init ---------- //
+
 
 function drawstart(event) {
     ctx.beginPath()
     ctx.moveTo(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop)
-    drawing = false
+    drawing = true
     lines[event.identifier] = {
         x: event.pageX - canvas.offsetLeft,
         y: event.pageY - canvas.offsetTop
@@ -36,13 +57,13 @@ function drawstart(event) {
 }
 
 function drawend(event) {
-    if (drawing) return;
+    if (!drawing) return;
     drawmove(event);
-    drawing = true;
+    drawing = false;
 }
 
 function drawmove(event) {
-    if (drawing) return;
+    if (!drawing) return;
     ctx.moveTo(lines[event.identifier].x, lines[event.identifier].y)
     ctx.lineTo(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop);
     ctx.stroke();
@@ -52,11 +73,19 @@ function drawmove(event) {
     }
 }
 
-function preventDrag(event) {
+function touchstart(event) { Array.from(event.touches).forEach(e => drawstart(e)) }
+
+function touchend(event) { Array.from(event.changedTouches).forEach(e => drawend(e)) }
+
+function touchmove(event) {
     event.preventDefault();
     Array.from(event.touches).forEach(e => drawmove(e));
 }
 
-canvas.addEventListener("touchstart", (event) => Array.from(event.touches).forEach(e => drawstart(e)), false)
-canvas.addEventListener("touchend", (event) => Array.from(event.changedTouches).forEach(e => drawend(e)), false)
-canvas.addEventListener("touchmove", preventDrag, false)
+canvas.addEventListener("touchstart", touchstart, false)
+canvas.addEventListener("touchend", touchend, false)
+canvas.addEventListener("touchmove", touchmove, false)
+
+canvas.addEventListener("mousedown", drawstart)
+canvas.addEventListener("mouseup", drawend)
+canvas.addEventListener("mousemove", drawmove)
