71492636e085324fe83fe0a58f2ce60e49140cb4
Edik Chekhanovsky <edichek12@hotmail.com>
2022-07-11 11:22:11 -0700

    Player and grid moved to top of dropdowns

--- get_assets.py
+++ get_assets.py
@@ -4,6 +4,9 @@ import os
 
 def add_option(soup, acc, path):
     for item in os.listdir(path):
+        if not os.path.isfile(os.path.join(path, item)):
+            continue
+
         item = item.replace("'", "\\'").replace(" ", "\ ")
         panel = soup.new_tag("div", attrs={"class": "panel"})
         option = soup.new_tag("button", attrs={
@@ -23,11 +26,15 @@ def main():
 
     map_path = "Assets/Maps"
     creature_path = "Assets/Creatures"
+    player_path = "Assets/Creatures/Players"
+    grid_path = "Assets/Maps/Grid"
     creatures = soup.find(id="creatures")
     maps = soup.find(id="maps")
 
     add_option(soup, creatures, creature_path)
+    add_option(soup, creatures, player_path)
     add_option(soup, maps, map_path)
+    add_option(soup, maps, grid_path)
 
     f = open("index.html", "w")
     f.write(soup.prettify())
