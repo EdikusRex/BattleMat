0a56de7276746d85452c3b11a2d71bf2f81a10ba
Scott Simenel <scott.simenel@gmail.com>
2024-07-28 21:23:04 -0700

    Add server set-up logic to server.js and reference socket.io client dependencies in html files.

---
+++ server.js
@@ -0,0 +1,40 @@
+let host = 3000
+
+// Using express: http://expressjs.com/
+var express = require('express');
+// Create the app
+var app = express();
+
+// Set up the server
+// process.env.PORT is related to deploying on heroku
+var server = app.listen(host, listen);
+
+// This call back just tells us that the server has started
+function listen() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://' + host + ':' + port);
+}
+
+app.use(express.static('public'));
+
+
+// WebSocket Portion
+// WebSockets work with the HTTP server
+var io = require('socket.io')(server);
+
+// Register a callback function to run when we have an individual connection
+// This is run for each individual user that connects
+io.sockets.on('connection',
+  // We are given a websocket object in our function
+  function (socket) {
+  
+    console.log("We have a new client: " + socket.id);
+  
+    // TODO: Add propagation for user socket emissions
+    
+    socket.on('disconnect', function() {
+      console.log("Client has disconnected");
+    });
+  }
+);
